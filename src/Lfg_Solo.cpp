0bf13bd0a4a0a1f081fb5800fe8425ad19695bca
Micrah <mic_24@live.com>
2019-04-27 16:35:14 +1000

    first commit

---
+++ src/Lfg_Solo.cpp
@@ -0,0 +1,32 @@
+/*
+** Made by Traesh https://github.com/Traesh
+** AzerothCore 2019 http://www.azerothcore.org/
+** Made into a module by Micrah https://github.com/milestorme/
+*/
+
+#include "Config.h"
+#include "World.h"
+#include "LFGMgr.h"
+#include "Chat.h"
+
+class spp_lfg_solo : public PlayerScript
+{
+public:
+    spp_lfg_solo() : PlayerScript("spp_lfg_solo") { }
+
+    void OnLogin(Player* player, bool /*loginFirst*/)
+    {
+        if (sConfigMgr->GetBoolDefault("LFG.SoloMode", true))
+        {
+            if (!sLFGMgr->IsTesting())
+            {
+                sLFGMgr->ToggleTesting();
+            }
+        }
+    }
+};
+
+void AddSC_LFG_SoloMode()
+{
+    new spp_lfg_solo;
+}
---
+++ src/loader.h
@@ -0,0 +1,1 @@
+void AddSpp_Lfg_SoloScripts();
